720b9e793bd74e7e954e6fc417c29b56709eaa15
Akshay Sahu <akki5233@gmail.com>
2020-10-18 22:40:00 +0530

    Merge sort practice- Random folder

---
+++ Random/Merge Sort.py
@@ -0,0 +1,52 @@
+##### MERGE SORT Practice #####
+
+def mergeSort(arr):
+
+    if len(arr) > 1:
+
+        mid = len(arr)//2
+
+        L = arr[:mid]
+        R = arr[mid:]
+
+        mergeSort(L)
+        mergeSort(R)
+
+        i = j = k = 0
+
+        while i < len(L) and j < len(R):
+
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+def printlist(arr):
+    for i in range(len(arr)):
+        print(arr[i], end = " ")
+    print()
+
+if __name__ == "__main__":
+    print("Enter the array to be sorted, seperated by space: ")
+    arr = list(map(int,input().split()))
+    print("The input array is: ")
+    printlist(arr)
+    mergeSort(arr)
+    print("Sorted array is: ")
+    printlist(arr)
+
+
+
