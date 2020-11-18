1d6f149f5009be9e06716b923848983b44d72e46
Akshay Sahu <akki5233@gmail.com>
2020-11-18 12:37:41 +0530

    Minimum path sum

---
+++ Leetcode/Medium/Minimum path sum.py
@@ -0,0 +1,21 @@
+## https://leetcode.com/problems/minimum-path-sum
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if i == 0 and j == 0:
+                    continue
+                if i == 0:
+                    grid[i][j] = grid[i][j] + grid[i][j-1]
+                
+                elif j == 0:
+                    grid[i][j] = grid[i][j] + grid[i-1][j]
+                    
+                else:
+                    grid[i][j] = grid[i][j] + min([grid[i-1][j],grid[i][j-1]])
+            
+        return grid[-1][-1]
+            
+        
