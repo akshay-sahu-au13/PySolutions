aaad4cd52484b4a96fe73ff7db68be3c25b7e9f5
Akshay Sahu <akki5233@gmail.com>
2020-11-30 18:37:39 +0530

    Array DS- Hackerrrank

---
+++ Hackerrank/Problem_Solving/Array-ds.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+## https://www.hackerrank.com/challenges/arrays-ds/problem
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the reverseArray function below.
+def reverseArray(a):
+    # a = a[::-1]
+    # return a
+    n = len(a)
+    i = 0
+    while i < n//2:
+        a[i],a[n-i-1] = a[n-i-1],a[i]
+        i += 1
+    return a
+    
+    
+    
+    
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
