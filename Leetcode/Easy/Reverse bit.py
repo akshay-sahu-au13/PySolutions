3ae9498d93bab2dd0fc4e848b1aa9af991d5dc79
Akshay Sahu <akki5233@gmail.com>
2020-12-03 14:03:24 +0530

    Leetcode 190: Reverse Bits

---
+++ Leetcode/Easy/Reverse bit.py
@@ -0,0 +1,8 @@
+## https://leetcode.com/problems/reverse-bits/
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        reverse = bin(n)[::-1][:-2]
+        reverse += "0"*(32-len(reverse))
+        return int(reverse,2)
+        
