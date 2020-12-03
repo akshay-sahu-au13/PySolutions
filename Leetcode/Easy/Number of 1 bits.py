616009861d5e9133547624aa420d801c38524a4d
Akshay Sahu <akki5233@gmail.com>
2020-12-03 13:45:53 +0530

    Leetcode 191: No. of 1 bits

--- Leetcode/Easy/Number of 1 bits.py
+++ Leetcode/Easy/Number of 1 bits.py
@@ -3,3 +3,14 @@
 class Solution:
 	def hammingWeight(self, n: int) -> int:
 		return bin(n).count("1")
+
+####### Another approach ########
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        bitcount = 0
+        while n > 0:
+            if n % 2 == 1:
+                bitcount += 1
+            n >>= 1
+        return bitcount
