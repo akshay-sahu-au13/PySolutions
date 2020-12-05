df099efa67e07a01f3ebb12d4f6b037b4fc6633c
Akshay Sahu <akki5233@gmail.com>
2020-12-05 12:26:52 +0530

    Leetcode 338: Counting bts

--- Leetcode/Medium/Counting bits.py
+++ Leetcode/Medium/Counting bits.py
@@ -13,3 +13,20 @@ class Solution:
             _1bits.append(count)
             
         return _1bits
+
+### Memoization solution ###
+
+
+class Solution:
+    def countBits(self, num: int) -> List[int]:
+        if num == 0:
+            return [0]
+        if num == 1:
+            return [0,1]
+        result = [0, 1]
+        for n in range(2,num+1):
+            if n % 2 == 0:
+                result.append(result[int(n/2)])
+            else:
+                result.append(result[int(n/2)]+1)
+        return result
