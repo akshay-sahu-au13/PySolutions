af9a88cd83b86f89e553a7155b83e84a8b7ef3a6
Akshay Sahu <akki5233@gmail.com>
2020-12-18 22:25:33 +0530

    30 days to code - Linked list-remove duplicates

---
+++ Hackerrank/Problem_Solving/Remove duplicates in linked list.py
@@ -0,0 +1,52 @@
+## https://www.hackerrank.com/challenges/30-linked-list-deletion/problem
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def insert(self,head,data):
+            p = Node(data)           
+            if head==None:
+                head=p
+            elif head.next==None:
+                head.next=p
+            else:
+                start=head
+                while(start.next!=None):
+                    start=start.next
+                start.next=p
+            return head  
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def removeDuplicates(self,head):
+        #Write your code here
+        if head is None and head.next is None:
+            return head
+        data = [head.data]
+        temp = head
+        temp1 = head.next
+        while temp and temp1:
+            if temp1.data in data:
+                temp.next = temp1.next
+                temp1 = temp1.next
+            else:
+                data.append(temp1.data)
+                temp = temp1
+                temp1 = temp.next
+            
+        return head
+            
+            
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+head=mylist.removeDuplicates(head)
+mylist.display(head); 
