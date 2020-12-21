2b319a7da63a9d3a956ab44f55a91ac60cb8cdf9
Akshay Sahu <akki5233@gmail.com>
2020-12-21 22:04:57 +0530

    Leetcode - Longest Valid parentheses

---
+++ Leetcode/Hard/Longest valid parentheses.py
@@ -0,0 +1,36 @@
+## https://leetcode.com/problems/longest-valid-parentheses/
+
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        
+        l = r = maxLength = 0
+        
+        for i in range(len(s)):
+            
+            if s[i] == "(":
+                l += 1
+            else:
+                r += 1
+                
+            if l == r:
+                maxLength = max(maxLength, r*2)
+            elif r > l:
+                l = r = 0
+        
+        l = r = 0
+        
+        for i in range(len(s) - 1, -1, -1): 
+
+            if (s[i] == '('): 
+                l += 1
+            else: 
+                r += 1
+
+            if (l == r): 
+                maxLength = max(maxLength, 2 * l) 
+
+            elif (l > r): 
+                l = r = 0
+
+            
+        return maxLength
